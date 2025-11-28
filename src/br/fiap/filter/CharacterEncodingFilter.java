7c7409a3a32f7b6dec593783fadd36d76b8b3f23
Guilherme Costa Proença <guilherme.proenca134@gmail.com>
2025-11-28 17:19:50 -0300

    Corrige CharacterEncodingFilter para não interferir com arquivos estáticos (CSS, JS, imagens)

--- src/br/fiap/filter/CharacterEncodingFilter.java
+++ src/br/fiap/filter/CharacterEncodingFilter.java
@@ -1,6 +1,7 @@
 package br.fiap.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class CharacterEncodingFilter implements Filter {
@@ -15,8 +16,36 @@ public class CharacterEncodingFilter implements Filter {
             throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
+        
+        // Verifica se é uma requisição HTTP
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String requestURI = httpRequest.getRequestURI();
+            
+            // Não aplica content-type text/html para arquivos estáticos
+            if (!isStaticResource(requestURI)) {
+                response.setContentType("text/html; charset=UTF-8");
+            }
+        } else {
+            response.setContentType("text/html; charset=UTF-8");
+        }
+        
         chain.doFilter(request, response);
+    }
+    
+    private boolean isStaticResource(String uri) {
+        return uri.endsWith(".css") || 
+               uri.endsWith(".js") || 
+               uri.endsWith(".png") || 
+               uri.endsWith(".jpg") || 
+               uri.endsWith(".jpeg") || 
+               uri.endsWith(".gif") || 
+               uri.endsWith(".svg") || 
+               uri.endsWith(".ico") || 
+               uri.endsWith(".woff") || 
+               uri.endsWith(".woff2") || 
+               uri.endsWith(".ttf") || 
+               uri.endsWith(".eot");
     }
 
     @Override
@@ -24,4 +53,6 @@ public class CharacterEncodingFilter implements Filter {
         // Limpeza do filtro
     }
 }
+
+
 
